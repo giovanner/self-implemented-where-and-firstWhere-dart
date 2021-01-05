af0e45a62e56aa0ac58a17773b22f3d7d3d17319
Giovanne Ramos <giovanneramos@Giovannes-MacBook-Pro.local>
2021-01-05 15:59:02 -0300

    Version 1.0 initial commit

---
+++ main.dart
@@ -0,0 +1,40 @@
+const listInts = [1, 2, 3, 4];
+
+void main() {
+  var pares = where<int>(listInts, (x) => x % 2 == 0);
+  var impares = where<int>(listInts, (x) => x % 2 != 0);
+  var procurando5 = firstWhere<int>(listInts, (x)=> x == 5, orElse:()=> -1);
+  var procurando2 = firstWhere<int>(listInts, (x)=> x == 2, orElse:()=> -1);
+  
+  
+  
+  print('pares $pares');
+  print('impares $impares');
+  print('First where procurando 5 $procurando5');
+  print('First where procurando 2 $procurando2');
+}
+
+
+
+T firstWhere<T>(List<T> itens, bool Function(T) f , {required T Function() orElse}){
+ 
+      
+    for(var iten in itens){
+      if(f(iten)){
+        return iten;
+      }
+    }
+    return orElse();
+}
+
+
+List<T> where<T>(List<T> itensToFilter, bool Function(T) f) {
+  var newList = <T>[];
+
+  itensToFilter.forEach((item) => {
+        if (f(item)) {newList.add(item)}
+      });
+  return newList;
+}
+
+
